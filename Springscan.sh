3edfe667e78ea2d1b17ba86226fd2c97afecbc58
PREMKUMAR NADARADJANE <premkumar.1990@gmail.com>
2023-11-15 19:35:34 +0100

    Update Springscan.sh

--- Springscan.sh
+++ Springscan.sh
@@ -11,20 +11,45 @@ output_file="spring_versions_table.txt"
 email_recipient="your@email.com"
 email_subject="Spring Versions Report"
 
-# Function to extract Spring and its dependencies from a WAR file
+# Function to extract Spring and its dependencies from a ZIP, WAR, or TAR file
 get_spring_versions() {
-  war_file="$1"
+  file="$1"
   temp_dir=$(mktemp -d)
-  
-  # Extract the contents of the WAR file
+
+  # Check if the file is a ZIP, WAR, or TAR
+  if unzip -t "$file" &> /dev/null; then
+    # It's a ZIP file
+    unzip -q "$file" -d "$temp_dir"
+  elif tar tf "$file" &> /dev/null; then
+    # It's a TAR file
+    tar xf "$file" -C "$temp_dir"
+  elif [ "${file##*.}" == "war" ]; then
+    # It's a WAR file
+    unzip -q "$file" -d "$temp_dir"
+  else
+    echo "Unsupported file format: $file"
+    rm -rf "$temp_dir"
+    return
+  fi
+
+  # Check if a WAR file is found
+  war_file=$(find "$temp_dir" -name "*.war" -print -quit)
+
+  if [ -z "$war_file" ]; then
+    echo "Invalid URL: No WAR file found in $file"
+    rm -rf "$temp_dir"
+    return
+  fi
+
+  # Extract the found WAR file
   unzip -q "$war_file" -d "$temp_dir"
-  
-  # Search for Spring and its dependencies
-  spring_versions=$(grep -o -E 'spring-framework-(.*?).jar' -r "$temp_dir" | sed 's/.*-\(.*\).jar/\1/' | sort -u)
-  
+
+  # Search for Spring JARs specifically in WEB-INF/lib
+  spring_versions=$(grep -o -E 'WEB-INF/lib/spring-framework-(.*?).jar' -r "$temp_dir" | sed 's/.*-\(.*\).jar/\1/' | sort -u)
+
   # Clean up temporary directory
   rm -rf "$temp_dir"
-  
+
   if [ -n "$spring_versions" ]; then
     echo "$spring_versions"
   else
@@ -35,14 +60,14 @@ get_spring_versions() {
 # Process each URL in the file
 while IFS= read -r url; do
   # Get the component name from the URL (assuming it ends with the component name)
-  component_name=$(basename "$url" | sed 's/\.war$//')
-  
-  # Download the WAR file
-  wget -q "$url" -O "$component_name.war"
-  
-  # Get Spring versions from the downloaded WAR file
-  spring_versions=$(get_spring_versions "$component_name.war")
-  
+  component_name=$(basename "$url" | sed 's/\.[^.]*$//')
+
+  # Download the ZIP, WAR, or TAR file
+  wget -q "$url" -O "$component_name.file"
+
+  # Get Spring versions from the downloaded file
+  spring_versions=$(get_spring_versions "$component_name.file")
+
   # Append the information to the output file
   echo -e "$component_name\t$spring_versions" >> "$output_file"
 done < "$url_file"
@@ -50,5 +75,5 @@ done < "$url_file"
 # Email the report
 cat "$output_file" | mail -s "$email_subject" "$email_recipient"
 
-# Clean up downloaded WAR files
-rm -f *.war
+# Clean up downloaded files
+rm -f *.file
